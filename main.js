ab7df50af70ffcaa331d8b07cf30cef632711106
Shivank254 <71582134+Shivank254@users.noreply.github.com>
2020-12-16 09:56:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,29 @@ if(screen.width < 2560){
     document.body.style.overflow = "hidden";
 }
 
+canvas.addEventListener("mousemove", my_mousemove);
 
+function my_mousemove(e){
+
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+    if(mouseEvent == "mouseDown") {
+        
+        ctx.beginPath();
+
+        ctx.strokeStyle = color;
+
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+
+        ctx.stroke();
+
+    }
+    
+}
 
     canvas.addEventListener("touchstart", my_touchstart);
     
